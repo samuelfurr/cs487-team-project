06dc19025382c941612dd4a17f47dd01bc87f203
Samuel Furr <sfurr@dhcp50.merunorth109.iit.edu>
2018-04-06 18:02:37 -0500

    Commiting task model, more of a check then anything

--- student_assist/cal/models.py
+++ student_assist/cal/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    name = models.CharField(primary_key=True, max_length=255)
+    date = models.DateTimeField()
+    due_date = models.DateTimeField()
+    duration = models.DurationField()
+    elapsed = models.DurationField()
+    
