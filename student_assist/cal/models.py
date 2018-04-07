9633bef81ffc83b1f812d52d0376a3293cc5a268
Samuel Furr <sfurr@dhcp50.merunorth109.iit.edu>
2018-04-07 13:59:16 -0500

    Commiting some very preliminary models for the pseudo-code writeup

--- student_assist/cal/models.py
+++ student_assist/cal/models.py
@@ -1,10 +1,24 @@
 from django.db import models
+from datetime import timedelta
 
 # Create your models here.
 class Task(models.Model):
-    name = models.CharField(primary_key=True, max_length=255)
+    task_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
     date = models.DateTimeField()
+    description = models.CharField(max_length=255, default="None")
     due_date = models.DateTimeField()
     duration = models.DurationField()
-    elapsed = models.DurationField()
-    
+    all_day = models.NullBooleanField()
+
+    def clean(self):
+        if self.all_day and self.duration != timedelta(days=1):
+            raise ValidationError("All day events must be 1 day long!")
+
+class Break(models.Model):
+   task = models.ForeignKey('Task', on_delete=models.CASCADE, primary_key=True)
+   date = models.DateTimeField()
+   duration = models.DurationField()
+
+   class Meta:
+       unique_together = (('task', 'date'),)
--- student_assist/feed/models.py
+++ student_assist/feed/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Quote(models.Model):
+    quote_id = models.AutoField(primary_key=True)
+    text = models.TextField(default="")
+    author = models.CharField(max_length=255)
+    last_used = models.DateTimeField()
